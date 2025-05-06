b8215dd9a3aa51d951787a2b653ac0628b7274b6
João Lucas <joaolucaspachecoab@gmail.com>
2025-05-06 11:35:13 -0300

    Style Posts Page

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -4,6 +4,8 @@ import { AppDispatch, RootState } from '../state/store'
 import { Post, pullFromServer, deletePost } from '../state/posts/postsSlice'
 import NewPostModal from './NewPostModal'
 import UpdatePostModal from './UpdatePostModal'
+import "./Posts.css"
+import { logout } from '../state/user/userSlice'
 
 const Posts = () => {
     const posts = useSelector((state: RootState) => state.posts.posts)
@@ -28,36 +30,53 @@ const Posts = () => {
         }
     }
 
-    return (
-        <div>
-        <div>{posts.map((post, index)=>(
-            <div key={index} className="post">
-                <div>{post.username}</div>
-                <div>{post.title}</div>
-                <div>{post.content}</div>
-                <div>{post.id}</div>
-                <button onClick={() => {
-                    setClickedPost(post)
-                    setShowUpdateModal(true)
-                }}>Update</button>
-                {post.username === username && <button  onClick={() => {
-                    handleDeletePost(post.id ||"")
-                }}>Delete</button>}
-            </div>
-        ))}</div>
-        <button onClick={handleNewPost}>New Post</button>
+    const handleLogout = () => {
+        dispatch(logout())
+    }
 
+    return (
+        <>
+        <nav className="navbar">
+            <button className='new-post-button'onClick={handleNewPost}>New Post</button>
+            <button className='logout-button' onClick={handleLogout}> Logout</button>
+        </nav>
+        <div className="posts-container">
+            <div className="posts-list">
+                {posts.map((post, index)=>(
+                    <div key={index} className="post" >
+                        <p className='post-title'>{post.title}</p>  
+                        <p className='post-user'>{post.username}</p>
+                        <p className='post-content'>{post.content}</p>
+                        <div className="button-div">
+                        {post.username === username && <button
+                            className='update-button'
+                            onClick={() => {
+                                setClickedPost(post)
+                                setShowUpdateModal(true)
+                            }}>Update</button>}
+                            
+                        {post.username === username && <button
+                            className='delete-button'
+                            onClick={() => {
+                                handleDeletePost(post.id ||"")
+                            }}>Delete</button>}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        </div>
         <NewPostModal 
           show={showNewModal}
           user={username}
           onClose={() => setShowNewModal(false)}
-        />
+          />
         <UpdatePostModal
           show={showUpdateModal}
           post={clickedPost}
           onClose={() => setShowUpdateModal(false)}
         />
-        </div>
+        </>
     )
 }
 
